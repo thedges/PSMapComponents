83c33a1912cae5abbb2aac6aa666d8001fae5648
Troy Hedges <thedges@salesforce.com>
2019-04-26 10:05:04 -0500

    PSRecordLocator updates

--- src/aura/PSRecordLocator/PSRecordLocatorController.js
+++ src/aura/PSRecordLocator/PSRecordLocatorController.js
@@ -1,118 +1,145 @@
 ({
-  jsLoaded: function(component, event, helper) {
-    console.log("jsLoaded called");
+  jsLoaded: function (component, event, helper) {
+    console.log ('jsLoaded called');
 
     //////////////////////////////////////
     // set default map lat/lng location //
     //////////////////////////////////////
-    component.set("v.latitude", component.get("v.mapCenterLat"));
-    component.set("v.longitude", component.get("v.mapCenterLng"));
+    component.set ('v.latitude', component.get ('v.mapCenterLat'));
+    component.set ('v.longitude', component.get ('v.mapCenterLng'));
 
     //////////////////////////////////////
     // get the current lat/lng location //
     //////////////////////////////////////
-    navigator.geolocation.getCurrentPosition($A.getCallback(function(location) {
-      component.set("v.origLat", location.coords.latitude);
-      component.set("v.origLng", location.coords.longitude);
-        
-      //var target = component.find("locateDiv");
-      //$A.util.removeClass(target, 'hide');
-    }));
+    navigator.geolocation.getCurrentPosition (
+      $A.getCallback (function (location) {
+        console.log ('getCurrentPosition...');
+        component.set ('v.origLat', location.coords.latitude);
+        component.set ('v.origLng', location.coords.longitude);
+
+        var target = component.find ('locateDiv');
+        $A.util.removeClass (target, 'hide');
+      })
+    );
 
     //////////////////////////////////////////////
     // load the current record lat/lng location //
     //////////////////////////////////////////////
-    helper.loadRecordLocation(component);
+    helper.loadRecordLocation (component);
   },
-  saveLocation: function(component, event, helper) {
-    console.log('saveLocation called...');
-
+  saveLocation: function (component, event, helper) {
+    console.log ('saveLocation called...');
 
     ////////////////////////////////////////////////////
     // create a parameter list to send to Apex method //
     ////////////////////////////////////////////////////
     var map = {};
 
-    map['recordId'] = component.get('v.recordId');
-    map['latitude'] = component.get('v.latitude');
-    map['longitude'] = component.get('v.longitude');
-    map['fullAddress'] = component.get('v.fullAddress');
-    map['street'] = component.get('v.street');
-    map['city'] = component.get('v.city');
-    map['state'] = component.get('v.state');
-    map['postal'] = component.get('v.postal');
+    map['recordId'] = component.get ('v.recordId');
+    map['latitude'] = component.get ('v.latitude');
+    map['longitude'] = component.get ('v.longitude');
+    map['fullAddress'] = component.get ('v.fullAddress');
+    map['street'] = component.get ('v.street');
+    map['city'] = component.get ('v.city');
+    map['state'] = component.get ('v.state');
+    map['postal'] = component.get ('v.postal');
 
-    map['mapLatField'] = component.get('v.mapLatField');
-    map['mapLngField'] = component.get('v.mapLngField');
-    map['addrField'] = component.get('v.addrField');
-    map['addrStreetField'] = component.get('v.addrStreetField');
-    map['addrCityField'] = component.get('v.addrCityField');
-    map['addrStateField'] = component.get('v.addrStateField');
-    map['addrPostalField'] = component.get('v.addrPostalField');
+    map['mapLatField'] = component.get ('v.mapLatField');
+    map['mapLngField'] = component.get ('v.mapLngField');
+    map['addrField'] = component.get ('v.addrField');
+    map['addrStreetField'] = component.get ('v.addrStreetField');
+    map['addrCityField'] = component.get ('v.addrCityField');
+    map['addrStateField'] = component.get ('v.addrStateField');
+    map['addrPostalField'] = component.get ('v.addrPostalField');
 
-    console.log('paramMap=' + JSON.stringify(map));
+    console.log ('paramMap=' + JSON.stringify (map));
 
     //////////////////////////////////////////////
     // setup call to the Apex controller method //
     //////////////////////////////////////////////
-    var action = component.get("c.saveRecordLocation");
-    action.setParams({
-      "params": JSON.stringify(map)
+    var action = component.get ('c.saveRecordLocation');
+    action.setParams ({
+      params: JSON.stringify (map),
     });
 
     //////////////////////////////////////////////////
     // setup the callback function for the response //
     //////////////////////////////////////////////////
-    action.setCallback(self, function(a) {
-      console.log(a.getReturnValue());
-      var resp = JSON.parse(a.getReturnValue());
+    action.setCallback (self, function (a) {
+      console.log (a.getReturnValue ());
+      var resp = JSON.parse (a.getReturnValue ());
 
       if (resp.status === 'SUCCESS') {
-        component.set('v.fullAddress', 'Location saved!');
-        var target = component.find("addressDiv");
+        component.set ('v.fullAddress', 'Location saved!');
+        var target = component.find ('addressDiv');
 
-        var markersLayerList = component.get("v.markersLayerList");
-        var markersLayer = markersLayerList[0];
-        markersLayer.clearLayers();
+        component.set ('v.recLat', component.get ('v.latitude'));
+        component.set ('v.recLng', component.get ('v.longitude'));
 
-        var crosshairIcon = L.icon({
-          //iconUrl: '/resource/mapCrosshair',
-          iconUrl: $A.get('$Resource.mapCrosshair4'),
-          iconSize: [200, 200] // size of the icon
-        });
+        var recordMarker = component.get ('v.recordMarker');
+        if (recordMarker != null)
+        {
+          recordMarker.setLatLng ([
+            component.get ('v.latitude'),
+            component.get ('v.longitude'),
+          ]);
 
-        var crosshair = new L.marker([parseFloat(component.get("v.latitude")), parseFloat(component.get("v.longitude"))], {
-          icon: crosshairIcon,
-          clickable: false
-        });
-        markersLayer.addLayer(crosshair);
+          component.set ('v.mode', 'RECORD');
+        }
+        else
+        {
+          var recordMarker = new L.marker ([
+            parseFloat (component.get ('v.latitude')),
+            parseFloat (component.get ('v.longitude')),
+          ]);
+          component.set ('v.recordMarker', recordMarker);
 
-        $A.util.addClass(target, 'hide');
-        $A.get('e.force:refreshView').fire();
+          var recordLayer = component.get("v.recordLayer");
+          recordLayer.addLayer (recordMarker);
+          component.set ('v.mode', 'RECORD');
+        }
+
+        //component.set ('v.mode', 'INIT');
+        helper.hideCenterCrosshair (component);
+
+        $A.get ('e.force:refreshView').fire ();
       } else {
-        component.set('v.fullAddress', resp.msg);
+        component.set ('v.fullAddress', resp.msg);
       }
     });
-    
+
     //////////////////////////////////
     // execute the Apex method call //
     //////////////////////////////////
-    $A.enqueueAction(action);
-
+    $A.enqueueAction (action);
   },
-  centerOnLocation: function(component, event, helper) {
-    console.log('centerOnLocation called...');
+  centerOnLocation: function (component, event, helper) {
+    console.log ('centerOnLocation called...');
     var self = this;
 
-    var map = component.get("v.map");
-    var lat = component.get("v.origLat");
-    var lng = component.get("v.origLng");
-    map.setView([lat, lng], component.get('v.mapZoomLevel'));
+    var map = component.get ('v.map');
+    var lat = component.get ('v.origLat');
+    var lng = component.get ('v.origLng');
+    map.setView ([lat, lng]);
 
-    helper.reverseGeocodeEsri(component, lat, lng, false);
+    component.set ('v.mode', 'TRACK');
+    helper.showCenterCrosshair (component);
   },
-  destroyCmp : function (component, event, helper) {
-        console.log('destroyCmp invoked...');
-        //component.destroy();
-    }
-})
+  centerOnRecord: function (component, event, helper) {
+    console.log ('centerOnRecord called...');
+    var self = this;
+
+    var map = component.get ('v.map');
+    var lat = component.get ('v.recLat');
+    var lng = component.get ('v.recLng');
+    console.log ('centerOnRecord lat=' + lat + ' lng=' + lng);
+    map.setView ([lat, lng]);
+
+    component.set ('v.mode', 'INIT');
+    helper.hideCenterCrosshair (component);
+  },
+  destroyCmp: function (component, event, helper) {
+    console.log ('destroyCmp invoked...');
+    //component.destroy();
+  },
+});
--- src/aura/PSRecordLocator/PSRecordLocatorHelper.js
+++ src/aura/PSRecordLocator/PSRecordLocatorHelper.js
@@ -1,220 +1,253 @@
 ({
-  loadRecordLocation: function(component) {
+  loadRecordLocation: function (component) {
     var self = this;
-    console.log('loadRecordLocation invoked...');
+    console.log ('loadRecordLocation invoked...');
 
-    self.createMap(component);
-      
+    self.createMap (component);
+    console.log ('map created...');
+
     var paramMap = {};
 
-    paramMap['recordId'] = component.get('v.recordId');
-    paramMap['mapLatField'] = component.get('v.mapLatField');
-    paramMap['mapLngField'] = component.get('v.mapLngField');     
+    paramMap['recordId'] = component.get ('v.recordId');
+    paramMap['mapLatField'] = component.get ('v.mapLatField');
+    paramMap['mapLngField'] = component.get ('v.mapLngField');
 
-    var action = component.get("c.getCurrLocation");
-    action.setParams({
-      "params": JSON.stringify(paramMap)
+    var action = component.get ('c.getCurrLocation');
+    action.setParams ({
+      params: JSON.stringify (paramMap),
     });
 
-    action.setCallback(self, function(a) {
-      console.log(a.getReturnValue());
-      var resp = JSON.parse(a.getReturnValue());
+    action.setCallback (self, function (a) {
+      console.log ('loadRecordLocation=' + a.getReturnValue ());
+      var resp = JSON.parse (a.getReturnValue ());
 
-      var map = component.get('v.map');
+      var map = component.get ('v.map');
       if (resp.status === 'ERROR') {
-        console.log('getCurrentPosition');
+        console.log ('getCurrentPosition');
         var options = {
-           enableHighAccuracy: false,
-           timeout: 5000,
-           maximumAge: Infinity
+          enableHighAccuracy: false,
+          timeout: 5000,
+          maximumAge: Infinity,
         };
 
-        if (component.get("v.autoCenter"))
-        {
-        navigator.geolocation.getCurrentPosition($A.getCallback(function(location) {
-          component.set("v.latitude", location.coords.latitude);
-          component.set("v.longitude", location.coords.longitude);
-          component.set("v.mapZoomLevel", component.get('v.mapZoomLevel'));
+        if (component.get ('v.autoCenter')) {
+          navigator.geolocation.getCurrentPosition (
+            $A.getCallback (function (location) {
+              component.set ('v.latitude', location.coords.latitude);
+              component.set ('v.longitude', location.coords.longitude);
+              component.set (
+                'v.mapZoomLevel',
+                component.get ('v.mapZoomLevel')
+              );
 
-          //self.createMap(component);
-          try
-          {
-          map.setView([parseFloat(component.get("v.latitude")), parseFloat(component.get("v.longitude"))], component.get("v.mapZoomLevel"));
-          self.reverseGeocodeEsri(component, location.coords.latitude, location.coords.longitude, true);
-          }
-          catch (err)
-          {
-              // do nothing
-          }
-        }), 
-        function(err) {
-            console.warn(`ERROR(${err.code}): ${err.message}`);
-            //self.createMap(component);
-        });
-        } 
-          else
-          {
-              component.set("v.initFinished", true);  
-          }
+              //self.createMap(component);
+              try {
+                map.setView (
+                  [
+                    parseFloat (component.get ('v.latitude')),
+                    parseFloat (component.get ('v.longitude')),
+                  ],
+                  component.get ('v.mapZoomLevel')
+                );
+              } catch (err) {
+                // do nothing
+              }
+            }),
+            function (err) {
+              console.warn (`ERROR(${err.code}): ${err.message}`);
+            }
+          );
+        } else {
+        }
       } else {
-        console.log('data=' + resp.data);
-        component.set("v.latitude", resp.data.latitude);
-        component.set("v.longitude", resp.data.longitude);
+        console.log ('data=' + JSON.stringify (resp.data));
+        component.set ('v.latitude', resp.data.latitude);
+        component.set ('v.longitude', resp.data.longitude);
+        component.set ('v.recLat', resp.data.latitude);
+        component.set ('v.recLng', resp.data.longitude);
 
-        //self.createMap(component);
-        try
-        {
-            console.log('record zoom = ' + component.get("v.mapZoomLevel"));
-          map.setView([parseFloat(component.get("v.latitude")), parseFloat(component.get("v.longitude"))], component.get("v.mapZoomLevel"));
+        try {
+          console.log ('record zoom = ' + component.get ('v.mapZoomLevel'));
+          map.setView (
+            [
+              parseFloat (component.get ('v.latitude')),
+              parseFloat (component.get ('v.longitude')),
+            ],
+            component.get ('v.mapZoomLevel')
+          );
 
-          var markersLayerList = component.get("v.markersLayerList");
-          var markersLayer = markersLayerList[0];
+          var recordLayer = component.get ('v.recordLayer');
 
-          var crosshairIcon = L.icon({
-            //iconUrl: '/resource/mapCrosshair',
-            iconUrl: $A.get('$Resource.mapCrosshair4'),
-            iconSize: [200, 200] // size of the icon
-          });
-          console.log('setting crosshair center=' + map.getCenter());
-          var crosshair = new L.marker([parseFloat(component.get("v.latitude")), parseFloat(component.get("v.longitude"))], {
-            icon: crosshairIcon,
-            clickable: false
-          });
-          markersLayer.addLayer(crosshair);
-            
+          var recordMarker = new L.marker ([
+            parseFloat (component.get ('v.latitude')),
+            parseFloat (component.get ('v.longitude')),
+          ]);
+          component.set ('v.recordMarker', recordMarker);
 
-
+          recordLayer.addLayer (recordMarker);
+        } catch (err) {
+          // do nothing
         }
-        catch (err)
-        {
-            // do nothing
-        }
-        
       }
-
     });
 
     // Enqueue the action
-    $A.enqueueAction(action);
+    $A.enqueueAction (action);
   },
-  reverseGeocodeEsri: function(component, lat, lng, showAddress) {
-    console.log('reverseGeocodeEsri invoked...');
-    var action = component.get("c.reverseGeocodeEsri");
-    if (action)
-    {
-    action.setParams({
-      "lat": lat,
-      "lng": lng
-    });
-
-    action.setCallback(self, function(a) {
-      console.log('resp=' + a.getReturnValue());
-
-      var resp = JSON.parse(a.getReturnValue());
-
-      if (resp.hasOwnProperty('error')) {
-        component.set('v.fullAddress', resp.error.details[0]);
-      } else {
-        //component.set('v.fullAddress', resp.address.Match_addr);
-        var fullAddr = '';
-        if (resp.address.Address != null) fullAddr += resp.address.Address;
-        if (resp.address.City != null) fullAddr += ', ' + resp.address.City;
-        if (resp.address.Region != null) fullAddr += ', ' + resp.address.Region;
-        if (resp.address.Postal != null) fullAddr += ' ' + resp.address.Postal;
-        
-        component.set('v.fullAddress', fullAddr);
-          
-        component.set('v.street', resp.address.Address);
-        component.set('v.city', resp.address.City);
-        component.set('v.state', resp.address.Region);
-        component.set('v.postal', resp.address.Postal);
-
-        if (showAddress)
-        {
-          console.log('show address bar');
-          var target = component.find("addressDiv");
-          $A.util.removeClass(target, 'hide');
-        }
-          
-        component.set("v.initFinished", true);  
-      }
-
-    });
-
-    $A.enqueueAction(action);
-    //    s$A.clientService.runActions([action], this, function() {});
-  }
-  },
-  createMap: function(component) {
+  reverseGeocodeEsri: function (component, lat, lng) {
     var self = this;
-    var globalId = component.getGlobalId();
+    console.log ('reverseGeocodeEsri invoked...');
+    console.log ('mode=' + component.get ('v.mode'));
 
-    console.log("create map...");
-    console.log("globalId=" + globalId);
-     
-    var markersLayer = new L.LayerGroup();
-    var markersLayerList = [];
-    markersLayerList.push(markersLayer);
+    var action = component.get ('c.reverseGeocodeEsri');
+    if (action) {
+      action.setParams ({
+        lat: lat,
+        lng: lng,
+      });
+
+      action.setCallback (self, function (a) {
+        console.log ('resp=' + a.getReturnValue ());
+
+        var resp = JSON.parse (a.getReturnValue ());
+
+        if (resp.hasOwnProperty ('error')) {
+          component.set ('v.fullAddress', resp.error.details[0]);
+        } else {
+          //component.set('v.fullAddress', resp.address.Match_addr);
+          var fullAddr = '';
+
+          if (resp.address.Address != null && resp.address.Address.length > 0) {
+            if (resp.address.Address != null) fullAddr += resp.address.Address;
+            if (resp.address.City != null) fullAddr += ', ' + resp.address.City;
+            if (resp.address.Region != null)
+              fullAddr += ', ' + resp.address.Region;
+            if (resp.address.Postal != null)
+              fullAddr += ' ' + resp.address.Postal;
+
+            component.set ('v.fullAddress', fullAddr);
+
+            component.set ('v.street', resp.address.Address);
+            component.set ('v.city', resp.address.City);
+            component.set ('v.state', resp.address.Region);
+            component.set ('v.postal', resp.address.Postal);
+          } else {
+            component.set ('v.fullAddress', fullAddr);
+          }
+          
+
+          self.setMode (component);
+          console.log ('fullAddress=' + component.get ('v.fullAddress'));
+          console.log ('mode=' + component.get ('v.mode'));
+        }
+      });
+
+      $A.enqueueAction (action);
+      //    s$A.clientService.runActions([action], this, function() {});
+    }
+  },
+  createMap: function (component) {
+    var self = this;
+    var globalId = component.getGlobalId ();
+
+    console.log ('create map...');
+    console.log ('globalId=' + globalId);
 
     ////////////////
     // create map //
     ////////////////
-    var map = L.map(document.getElementById(globalId + '_map'), {
+    var map = L.map (document.getElementById (globalId + '_map'), {
       zoomControl: true,
       boxZoom: true,
       trackResize: true,
-      doubleClickZoom: true
+      doubleClickZoom: true,
     });
 
-    map.attributionControl.setPrefix('');
-    L.tileLayer(
-      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-        attribution: ''
-      }).addTo(map);
-      markersLayer.addTo(map);
+    map.attributionControl.setPrefix ('');
+    L.tileLayer (
+      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
+      {
+        attribution: '',
+      }
+    ).addTo (map);
 
     ///////////////////////////////////////////////////////////////////////////////
     // center map on record location or if doesn't exist, users current position //
     ///////////////////////////////////////////////////////////////////////////////
-    console.log('create map zoom = ' + component.get("v.mapZoomLevel"));
-    map.setView([parseFloat(component.get("v.latitude")), parseFloat(component.get("v.longitude"))], component.get("v.mapZoomLevel"));
+    console.log ('create map zoom = ' + component.get ('v.mapZoomLevel'));
+    map.setView (
+      [
+        parseFloat (component.get ('v.latitude')),
+        parseFloat (component.get ('v.longitude')),
+      ],
+      component.get ('v.mapZoomLevel')
+    );
 
+    //////////////////////////////
+    // setup layers and markers //
+    //////////////////////////////
+    console.log ('adding layers and markers...');
+    var recordLayer = new L.LayerGroup ();
+    var crosshairLayer = new L.LayerGroup ();
 
-    ///////////////////////////////////////////
-    // create cross to keep at center of map //
-    ///////////////////////////////////////////
-    var crosshairIcon = L.icon({
+    recordLayer.addTo (map);
+    crosshairLayer.addTo (map);
+
+    var crosshairIcon = L.icon ({
       //iconUrl: '/resource/mapCrosshair',
-      iconUrl: $A.get('$Resource.mapCrosshair'),
-      iconSize: [200, 200] // size of the icon
+      iconUrl: $A.get('$Resource.PSMapComponents') + '/mapCrosshair.png',
+      iconSize: [200, 200], // size of the icon
     });
-    console.log('setting crosshair center=' + map.getCenter());
-    var crosshair = new L.marker(map.getCenter(), {
+    console.log ('setting crosshair center=' + map.getCenter ());
+    var crosshairMarker = new L.marker (map.getCenter (), {
       icon: crosshairIcon,
-      clickable: false
+      clickable: false,
     });
-    crosshair.addTo(map);
 
-    map.on('move', function(e) {
-      crosshair.setLatLng(map.getCenter());
+    map.on ('move', function (e) {
+      var mode = component.get ('v.mode');
+      if (mode === 'RECORD' || mode === 'CENTER') {
+        component.set ('v.mode', 'TRACK');
+        self.showCenterCrosshair (component);
+      }
+      crosshairMarker.setLatLng (map.getCenter ());
     });
+    component.set ('v.crosshairMarker', crosshairMarker);
+
+    component.set ('v.crosshairLayer', crosshairLayer);
+    component.set ('v.recordLayer', recordLayer);
 
     //////////////////////////////////////////////////////////////////
     // when map is moved, do reverse address lookup and show on map //
     //////////////////////////////////////////////////////////////////
-    map.on('moveend', function(e) {
-      console.log('moveend=' + map.getCenter());
-      var coords = map.getCenter();
-      component.set('v.latitude', coords.lat);
-      component.set('v.longitude', coords.lng);
+    map.on ('moveend', function (e) {
+      console.log ('moveend=' + map.getCenter ());
 
-      console.log('calling reverseGeocodeEsri');
-      //self.reverseGeocodeEsri(component, coords.lat, coords.lng, component.get('v.initFinished'));
+      var coords = map.getCenter ();
+      component.set ('v.latitude', coords.lat);
+      component.set ('v.longitude', coords.lng);
+
+      console.log ('calling reverseGeocodeEsri');
+      self.reverseGeocodeEsri (component, coords.lat, coords.lng);
     });
 
-    component.set("v.map", map);
-    component.set("v.markersLayerList", markersLayerList);
-    console.log("map created...");
-
-  }
-})
+    component.set ('v.map', map);
+    console.log ('map created...');
+  },
+  showCenterCrosshair: function (component) {
+    var crosshairLayer = component.get ('v.crosshairLayer');
+    var crosshairMarker = component.get ('v.crosshairMarker');
+    crosshairLayer.clearLayers ();
+    crosshairLayer.addLayer (crosshairMarker);
+  },
+  hideCenterCrosshair: function (component) {
+    var crosshairLayer = component.get ('v.crosshairLayer');
+    crosshairLayer.clearLayers ();
+  },
+  setMode: function (component) {
+    var mode = component.get ('v.mode');
+    if (mode === 'INIT') {
+      component.set ('v.mode', 'RECORD');
+    }
+  },
+});
